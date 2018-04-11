e13563cbcc0aca5a54512154c5ea04ad10db5a9a
574413114 <574413114@qq.com>
2018-04-12 01:34:02 +0800

    导入Element组件库

---
+++ src/components/login/login.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+      <p>登陆组件</p>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data () {
+    return {
+      userForm: {
+        username: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    async login () {
+      const res = await axios.post('http://localhost:8888/api/private/v1/login', this.userForm)
+      const data = res.data
+      if (data.meta.status === 200) {
+        this.$router.push({
+          name: 'home'
+        })
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
